728f89550ef6e6a710b2f80bd0481e61168b4c47
Alexandre Stanislawski <ast@zenexity.com>
2012-01-29 20:09:02 +0100

    now sending speaker events to the server

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,8 @@ import play.api._
 import play.api.mvc._
 import models.LiveStream
 import scala.collection.immutable 
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
 
@@ -18,7 +20,20 @@ object Application extends Controller {
     Ok("initPrez")
   }
 
-  def broadCast = Action { request =>
+
+  val broadcastForm = Form(
+    tuple (
+      "id" -> number,
+      "page" -> nonEmptyText
+    )
+  )
+
+  def broadCast = Action { implicit request =>
+    val log = broadcastForm.bindFromRequest().fold(
+      formWithErrors => BadRequest("NOT GOOD! Try again"),
+      {case (id, page) => println("Page actuelle" + page)}
+    ) 
+    //println("ID "+id.)
     //To the clients 
     Ok("broadCast")
   }
@@ -47,6 +62,5 @@ object Application extends Controller {
         .headOption
         .map(url => Ok(views.html.speaker(5, url)))
         .getOrElse(BadRequest)
-
   }
 }
